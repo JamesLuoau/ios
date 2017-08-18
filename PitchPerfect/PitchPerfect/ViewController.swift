afa6fd1e544629abb0adfacfc81e7026aa13d6a1
James Luo <jluo@bglcorp.com.au>
2017-08-18 23:24:09 +1000

    added event

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,9 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(_ sender: Any, forEvent event: UIEvent) {
+        print("recording")
+        label.text = "recording"
+    }
 }
 
